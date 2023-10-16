a6e9dba6016885054159aacf63c694537bd481a4
Carlos Santos <4a.santos@gmail.com>
2023-10-16 11:29:10 +0200

    openvidu-basic-ruby to LiveKit

--- openvidu-basic-ruby/app.rb
+++ openvidu-basic-ruby/app.rb
@@ -4,12 +4,14 @@ require 'sinatra'
 require 'sinatra/cors'
 require 'faraday'
 require 'json'
+require 'livekit'
 require './env.rb'
 
 # Load env variables
 SERVER_PORT = ENV['SERVER_PORT']
-OPENVIDU_URL = ENV['OPENVIDU_URL']
-OPENVIDU_SECRET = ENV['OPENVIDU_SECRET']
+LIVEKIT_URL = ENV['LIVEKIT_URL']
+LIVEKIT_API_KEY = ENV['LIVEKIT_API_KEY']
+LIVEKIT_API_SECRET = ENV['LIVEKIT_API_SECRET']
 
 set :port, SERVER_PORT
 
@@ -18,47 +20,26 @@ set :allow_origin, "*"
 set :allow_methods, "POST,OPTIONS"
 set :allow_headers, "content-type"
 
-post '/api/sessions' do
-  begin
-    body = request.body.read
-    response = Faraday.post do |req|
-      req.url "#{OPENVIDU_URL}openvidu/api/sessions"
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "Basic #{Base64.encode64("OPENVIDUAPP:#{OPENVIDU_SECRET}").strip}"
-      req.body = body
-    end
-    if response.success?
-      (JSON.parse response.body)['sessionId']
-    else
-      if response.status == 409
-        # Session already exists in OpenVidu
-        (JSON.parse body)['customSessionId']
-      else
-        status response.status
-        body response.body
-      end
-    end
-  rescue Faraday::Error => err
-    err.response
-  end
-end
+post '/token' do
 
-post '/api/sessions/:sessionId/connections' do
-  begin
-    body = request.body.read
-    response = Faraday.post do |req|
-      req.url "#{OPENVIDU_URL}openvidu/api/sessions/#{params['sessionId']}/connection"
-      req.headers['Content-Type'] = 'application/json'
-      req.headers['Authorization'] = "Basic #{Base64.encode64("OPENVIDUAPP:#{OPENVIDU_SECRET}").strip}"
-      req.body = body
-    end
-    if response.success?
-      (JSON.parse response.body)['token']
-    else
-      status response.status
-      body response.body
-    end
-  rescue Faraday::Error => err
-    err.response
+  content_type :json
+
+  body = JSON.parse(request.body.read)
+  room_name = body['roomName']
+  participant_name = body['participantName']
+
+  if room_name.nil? || participant_name.nil?
+    status 400
+    return 'roomName and participantName are required'
   end
+
+  # By default, a token expires after 6 hours.
+  # You may override this by passing in ttl when creating the token. ttl is expressed in seconds.
+  token = LiveKit::AccessToken.new(api_key: LIVEKIT_API_KEY, api_secret: LIVEKIT_API_SECRET)
+  token.identity = participant_name
+  token.name = participant_name
+  token.add_grant(roomJoin: true, room: room_name)
+  token.metadata = { "livekitUrl" =>  LIVEKIT_URL}
+
+  token.to_jwt
 end
--- openvidu-basic-ruby/env.rb
+++ openvidu-basic-ruby/env.rb
@@ -1,3 +1,4 @@
 ENV['SERVER_PORT'] = '5000'
-ENV['OPENVIDU_URL'] = 'http://localhost:4443/'
-ENV['OPENVIDU_SECRET'] = 'MY_SECRET'
+ENV['LIVEKIT_URL'] = 'ws://localhost:7880/'
+ENV['LIVEKIT_API_KEY'] = 'devkey'
+ENV['LIVEKIT_API_SECRET'] = 'secret'
