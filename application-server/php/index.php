264e2e4e6fe48c03b9d3b329d834ddb578c1deac
pabloFuente <pablofuenteperez@gmail.com>
2024-05-16 18:30:22 +0200

    PHP: webhook receiver

--- application-server/php/index.php
+++ application-server/php/index.php
@@ -4,20 +4,20 @@ require __DIR__ . "/vendor/autoload.php";
 use Agence104\LiveKit\AccessToken;
 use Agence104\LiveKit\AccessTokenOptions;
 use Agence104\LiveKit\VideoGrant;
+use Agence104\LiveKit\WebhookReceiver;
 use Dotenv\Dotenv;
 
 Dotenv::createImmutable(__DIR__)->safeLoad();
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Methods: OPTIONS,POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $LIVEKIT_API_KEY = $_ENV["LIVEKIT_API_KEY"] ?? "devkey";
 $LIVEKIT_API_SECRET = $_ENV["LIVEKIT_API_SECRET"] ?? "secret";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && $_SERVER["PATH_INFO"] === "/token") {
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] === "POST" && $_SERVER["PATH_INFO"] === "/token") {
     $data = json_decode(file_get_contents("php://input"), true);
 
     $roomName = $data["roomName"] ?? null;
@@ -41,6 +41,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && $_SERVER["PATH_INFO"] === "/token")
 
     echo json_encode($token);
     exit();
+}
+
+$webhookReceiver = (new WebhookReceiver($LIVEKIT_API_KEY, $LIVEKIT_API_SECRET));
+
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER["REQUEST_METHOD"] === "POST" && $_SERVER["PATH_INFO"] === "/webhook") {
+    $headers = getallheaders();
+    $authHeader = $headers['Authorization'];
+    $body = file_get_contents("php://input");
+    try {
+        $event = $webhookReceiver->receive($body, $authHeader);
+        error_log('LiveKit Webhook:');
+        error_log(print_r($event->getEvent(), true));
+        exit();
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo "Error validating webhook event";
+        echo json_encode($e->getMessage());
+        exit();
+    }
 }
 
 echo json_encode("Unsupported endpoint or method");
