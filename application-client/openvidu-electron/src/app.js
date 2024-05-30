0e55ef702edca24b8ba1dce1abac87b2c438eb3f
juancarmore <juancar_more2@hotmail.com>
2024-05-30 20:01:09 +0200

    Update Electron tutorial

---
+++ application-client/openvidu-electron/src/app.js
@@ -0,0 +1,159 @@
+const { Room, RoomEvent } = require("livekit-client");
+
+// Configure this constants with correct URLs depending on your deployment
+const APPLICATION_SERVER_URL = "http://localhost:6080/";
+const LIVEKIT_URL = "ws://localhost:7880/";
+
+var room;
+
+async function joinRoom() {
+    // Initialize a new Room object
+    room = new Room();
+
+    // Specify the actions when events take place in the room
+    // On every new Track received...
+    room.on(RoomEvent.TrackSubscribed, (track, _publication, participant) => {
+        addTrack(track, participant.identity);
+    });
+
+    // On every new Track destroyed...
+    room.on(RoomEvent.TrackUnsubscribed, (track, _publication, participant) => {
+        track.detach();
+        document.getElementById(track.sid)?.remove();
+
+        if (track.kind === "video") {
+            removeVideoContainer(participant.identity);
+        }
+    });
+
+    try {
+        // Get the room name and participant name from the form
+        const roomName = document.getElementById("room-name").value;
+        const userName = document.getElementById("participant-name").value;
+
+        // Get a token from your application server with the room name and participant name
+        const token = await getToken(roomName, userName);
+
+        // Connect to the room with the LiveKit URL and the token
+        await room.connect(LIVEKIT_URL, token);
+
+        // Hide the 'Join room' page and show the 'Room' page
+        document.getElementById("room-title").innerText = roomName;
+        document.getElementById("join").hidden = true;
+        document.getElementById("room").hidden = false;
+
+        // Publish your camera and microphone
+        await room.localParticipant.enableCameraAndMicrophone();
+        const localVideoTrack = this.room.localParticipant.videoTrackPublications.values().next().value.track;
+        addTrack(localVideoTrack, userName, true);
+    } catch (error) {
+        console.log("There was an error connecting to the room:", error.message);
+        await leaveRoom();
+    }
+}
+
+function addTrack(track, participantIdentity, local = false) {
+    const element = track.attach();
+    element.id = track.sid;
+
+    /* If the track is a video track, we create a container and append the video element to it 
+    with the participant's identity */
+    if (track.kind === "video") {
+        const videoContainer = createVideoContainer(participantIdentity, local);
+        videoContainer.append(element);
+        appendParticipantData(videoContainer, participantIdentity + (local ? " (You)" : ""));
+    } else {
+        document.getElementById("layout-container").append(element);
+    }
+}
+
+async function leaveRoom() {
+    // Leave the room by calling 'disconnect' method over the Room object
+    await room.disconnect();
+
+    // Remove all HTML elements inside the layout container
+    removeAllLayoutElements();
+
+    // Back to 'Join room' page
+    document.getElementById("join").hidden = false;
+    document.getElementById("room").hidden = true;
+}
+
+window.onbeforeunload = () => {
+    room?.disconnect();
+};
+
+window.onload = generateFormValues;
+
+function generateFormValues() {
+    document.getElementById("room-name").value = "Test Room";
+    document.getElementById("participant-name").value = "Participant" + Math.floor(Math.random() * 100);
+}
+
+function createVideoContainer(participantIdentity, local = false) {
+    const videoContainer = document.createElement("div");
+    videoContainer.id = `camera-${participantIdentity}`;
+    videoContainer.className = "video-container";
+    const layoutContainer = document.getElementById("layout-container");
+
+    if (local) {
+        layoutContainer.prepend(videoContainer);
+    } else {
+        layoutContainer.append(videoContainer);
+    }
+
+    return videoContainer;
+}
+
+function appendParticipantData(videoContainer, participantIdentity) {
+    const dataElement = document.createElement("div");
+    dataElement.className = "participant-data";
+    dataElement.innerHTML = `<p>${participantIdentity}</p>`;
+    videoContainer.prepend(dataElement);
+}
+
+function removeVideoContainer(participantIdentity) {
+    const videoContainer = document.getElementById(`camera-${participantIdentity}`);
+    videoContainer?.remove();
+}
+
+function removeAllLayoutElements() {
+    const layoutElements = document.getElementById("layout-container").children;
+    Array.from(layoutElements).forEach((element) => {
+        element.remove();
+    });
+}
+
+/**
+ * --------------------------------------------
+ * GETTING A TOKEN FROM YOUR APPLICATION SERVER
+ * --------------------------------------------
+ * The method below request the creation of a token to
+ * your application server. This prevents the need to expose
+ * your LiveKit API key and secret to the client side.
+ *
+ * In this sample code, there is no user control at all. Anybody could
+ * access your application server endpoints. In a real production
+ * environment, your application server must identify the user to allow
+ * access to the endpoints.
+ */
+async function getToken(roomName, participantName) {
+    const response = await fetch(APPLICATION_SERVER_URL + "token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            roomName,
+            participantName
+        })
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(`Failed to get token: ${error.errorMessage}`);
+    }
+
+    const token = await response.json();
+    return token.token;
+}
--- application-client/openvidu-electron/src/index.js
+++ application-client/openvidu-electron/src/index.js
@@ -1,196 +1,50 @@
-const axios = require("axios");
-const { Room, RoomEvent } = require("livekit-client");
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+const livekit = require("livekit-client");
 
-const ipcRenderer = require("electron").ipcRenderer;
-const { BrowserWindow } = require("@electron/remote");
-var room;
-var myParticipantName;
-var myRoomName;
-var isScreenShared = false;
-var screenSharePublication;
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+    app.quit();
+}
 
-// Configure this constants with correct URLs depending on your deployment
-const APPLICATION_SERVER_URL = "http://localhost:6080/";
-const LIVEKIT_URL = "ws://localhost:7880/";
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
 
-ipcRenderer.on("screen-share-ready", async (event, sourceId) => {
-  if (sourceId) {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: "desktop",
-            chromeMediaSourceId: sourceId,
-          },
-        },
-      });
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
 
-      const track = stream.getVideoTracks()[0];
-      const screenPublication = await room.localParticipant.publishTrack(track);
-      isScreenShared = true;
-      screenSharePublication = screenPublication;
-      const element = screenPublication.track.attach();
-      element.id = screenPublication.trackSid;
-      element.className = "removable";
-      document.getElementById("local-participant").appendChild(element);
-    } catch (error) {
-      console.error("Error enabling screen sharing", error);
-    }
-  }
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow();
+
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
-async function joinRoom() {
-  myRoomName = document.getElementById("roomName").value;
-  myParticipantName = document.getElementById("participantName").value;
-
-  // --- 1) Get a Room object ---
-  room = new Room();
-
-  // --- 2) Specify the actions when events take place in the room ---
-
-  // On every new Track received...
-  room.on(RoomEvent.TrackSubscribed, (track, publication, participant) => {
-    console.log("TrackSubscribed", track, publication, participant);
-    const element = track.attach();
-    element.id = track.sid;
-    element.className = "removable";
-    document.getElementById("remote-participants").appendChild(element);
-  });
-
-  // On every new Track destroyed...
-  room.on(RoomEvent.TrackUnsubscribed, (track, publication, participant) => {
-    console.log("TrackUnSubscribed", track, publication, participant);
-
-    track.detach();
-    document.getElementById(track.sid)?.remove();
-  });
-
-  // --- 3) Connect to the room with a valid access token ---
-
-  // Get a token from the application backend
-  const token = await getToken(myRoomName, myParticipantName);
-
-  await room.connect(LIVEKIT_URL, token);
-
-  showRoom();
-  // --- 4) Publish your local tracks ---
-  await room.localParticipant.setMicrophoneEnabled(true);
-  const publication = await room.localParticipant.setCameraEnabled(true);
-  const element = publication.track.attach();
-  element.className = "removable";
-  document.getElementById("local-participant").appendChild(element);
-}
-
-async function toggleScreenShare() {
-  console.log("Toggling screen share");
-  const enabled = !isScreenShared;
-
-  if (enabled) {
-    openScreenShareModal();
-  } else {
-    // Disable screen sharing
-    await stopScreenSharing();
-  }
-}
-
-async function stopScreenSharing() {
-  try {
-    await room.localParticipant.unpublishTrack(screenSharePublication.track);
-    isScreenShared = false;
-    const trackSid = screenSharePublication?.trackSid;
-
-    if (trackSid) {
-      document.getElementById(trackSid)?.remove();
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
-    screenSharePublication = undefined;
-  } catch (error) {
-    console.error("Error stopping screen sharing", error);
-  }
-}
-
-function leaveRoom() {
-  // --- 5) Leave the room by calling 'disconnect' method over the Room object ---
-
-  room.disconnect();
-  // Removing all HTML elements with user's nicknames.
-  // HTML videos are automatically removed when leaving a Room
-  removeAllParticipantElements();
-  hideRoom();
-}
-
-function showRoom() {
-  document.getElementById("room-header").innerText = myRoomName;
-  document.getElementById("join").style.display = "none";
-  document.getElementById("room").style.display = "block";
-}
-
-function hideRoom() {
-  document.getElementById("join").style.display = "block";
-  document.getElementById("room").style.display = "none";
-}
-
-function removeAllParticipantElements() {
-  var elementsToRemove = document.getElementsByClassName("removable");
-  while (elementsToRemove[0]) {
-    elementsToRemove[0].parentNode.removeChild(elementsToRemove[0]);
-  }
-}
-
-function openScreenShareModal() {
-  let win = new BrowserWindow({
-    parent: require("@electron/remote").getCurrentWindow(),
-    modal: true,
-    minimizable: false,
-    maximizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true,
-      contextIsolation: false,
-    },
-    resizable: false,
-  });
-  require("@electron/remote")
-    .require("@electron/remote/main")
-    .enable(win.webContents);
-
-  win.setMenu(null);
-  // win.webContents.openDevTools();
-
-  var theUrl = "file://" + __dirname + "/modal.html";
-  win.loadURL(theUrl);
-}
-
-/**
- * --------------------------------------------
- * GETTING A TOKEN FROM YOUR APPLICATION SERVER
- * --------------------------------------------
- * The methods below request the creation of a Token to
- * your application server. This keeps your OpenVidu deployment secure.
- *
- * In this sample code, there is no user control at all. Anybody could
- * access your application server endpoints! In a real production
- * environment, your application server must identify the user to allow
- * access to the endpoints.
- *
- */
-async function getToken(roomName, participantName) {
-  try {
-    const response = await axios.post(
-      APPLICATION_SERVER_URL + "token",
-      {
-        roomName,
-        participantName,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    return response.data;
-  } catch (error) {
-    console.error("No connection to application server", error);
-  }
-}
+});
